2ffc6f8fd39b06cd63c434cb7974f022173184ab
Sridhar Ratnakumar <srid@srid.ca>
2022-11-04 22:18:56 -0400

    Add flake.nix to build PDF

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit self; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem = { self', pkgs, ... }: {
+        packages.default = pkgs.stdenvNoCC.mkDerivation rec {
+          name = "DaoFP.pdf";
+          src = self;
+          buildInputs = [
+            pkgs.coreutils
+            (pkgs.texlive.combine {
+              inherit (pkgs.texlive) scheme-full latex-bin latexmk pygmentex;
+            })
+            pkgs.python37Packages.pygments
+            pkgs.which
+          ];
+          phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+          buildPhase = ''
+            set -e
+            export PATH="${pkgs.lib.makeBinPath buildInputs}";
+            pdflatex -shell-escape DaoFP.tex
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp DaoFP.pdf $out/
+          '';
+        };
+      };
+    };
+}
