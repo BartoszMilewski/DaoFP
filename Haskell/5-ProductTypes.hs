c54ed4a8eb62070d76e903d7256885d7fb10cf67
Bartosz Milewski <bartosz@relisoft.com>
2026-04-14 14:38:57 +0200

    Edits

--- Haskell/5-ProductTypes.hs
+++ Haskell/5-ProductTypes.hs
@@ -1,3 +1,4 @@
+
 thrd :: (a, b, c) -> c
 thrd (_, _, c) = c
 
@@ -17,10 +18,20 @@ assoc ((a, b), c) = (a, (b, c))
 runit :: (a, ()) -> a
 runit (a, _) = a
 
-class Monoid m where
-  mappend :: (m, m) -> m
-  mempty  :: () -> m
+class Monoid' m where
+  mappend' :: (m, m) -> m
+  mempty'  :: () -> m
 
+-- class Semigroup a where
+--    (<>) :: a -> a -> a
+
+newtype Endo a = Endo (a -> a)
+
+instance Semigroup (Endo a) where
+  (Endo f) <> (Endo g) = Endo (f . g)
+
+instance Monoid (Endo a) where
+  mempty = Endo id
 
 -- Exercises
 
@@ -29,3 +40,21 @@ maybeAB (Left b) = (Nothing, b)
 maybeAB (Right (a, b)) = (Just a, b)
 -- Another possibility:
 -- maybeAB (Right (a, b)) = (Nothing, b)
+
+newtype All = All Bool
+
+instance Semigroup All where
+  (All True) <> (All True) = All True
+  _ <> _ = All False
+
+instance Monoid All where
+  mempty = All True
+
+newtype Any = Any Bool
+
+instance Semigroup Any where
+  (Any a) <> (Any b) = Any (a || b)
+  _ <> _ = Any True
+
+instance Monoid Any where
+  mempty = Any False
